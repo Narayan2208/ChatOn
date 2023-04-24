11e0d717fd85acc29503f7385eac7f8e232c52eb
narayan dutta <narayandutta2208@gmail.com>
2023-04-25 02:55:45 +0530

    chats routes created

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const {registerUser,authUser} = require("../controllers/userControllers")
+const {protect} = require("../middlewares/authMiddleware")
+const {registerUser,authUser,allUsers} = require("../controllers/userControllers")
 const router = express.Router();
 
-// router.route('/').post(registerUser)  Another way to do this
-router.post('/', registerUser)
+router.route('/').post(registerUser).get(protect,allUsers)
+// router.post('/', registerUser)
 router.post('/login', authUser)
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv")
 const { chats } = require("./data/data");
 const connectDB = require('./config/db');
 const userRoutes = require("./routes/userRoutes")
+const chatRoutes = require("./routes/chatRoutes")
 const {notFound,errorHandler} = require("./middlewares/errorMiddleware")
 dotenv.config();
 
@@ -14,6 +15,7 @@ res.send("API is Running")
 })
 
 app.use('/api/user', userRoutes)
+app.use('/api/chat', chatRoutes)
 app.use(notFound)
 app.use(errorHandler)
 
