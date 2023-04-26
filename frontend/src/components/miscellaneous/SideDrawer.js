1f8929b91426c5162a4ce73a98328049ae0e767b
narayan dutta <narayandutta2208@gmail.com>
2023-04-26 21:25:59 +0530

    api update

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -1,17 +1,68 @@
 import React, { useState } from 'react'
-import { Avatar, Box, Button,Menu,MenuButton,MenuDivider,MenuItem,MenuList,Text , Tooltip } from '@chakra-ui/react'
+import {Spinner ,useDisclosure,Avatar,DrawerHeader, Input,DrawerBody,Drawer,DrawerOverlay,DrawerContent, Box, Button,Menu,MenuButton,MenuDivider,MenuItem,MenuList,Text , Tooltip } from '@chakra-ui/react'
 import { BellIcon, ChevronDownIcon } from '@chakra-ui/icons'
 import { ChatState } from '../../Context/ChatProvider';
 import ProfileModal from './ProfileModal';
-
-
+import { useNavigate } from 'react-router';
+import { useToast } from "@chakra-ui/toast";
+import ChatLoading from '../ChatLoading';
+import UserListItem from '../userAvatar/UserListItem';
+import axios from 'axios'
 function SideDrawer() {
   const [search, setSearch] = useState("");
-  const [searchResult, setsearchResult] = useState([]);
+  const [searchResult, setSearchResult] = useState([]);
   const [loading, setLoading] = useState(false)
   const [loadingChat, setLoadingChat] = useState()
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  let navigate = useNavigate()
+  const {user} = ChatState();
 
-  const {user} = ChatState()
+  const toast = useToast();
+
+  const logoutHandler = ()=>{
+    localStorage.removeItem("userInfo");
+    navigate("/")
+  }
+  const handleSearch = async  () => {
+    if (!search) {
+      toast({
+        title: "Please Enter something in search",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top-left",
+      });
+      return;
+  }
+
+      try {
+        setLoading(true);
+  
+        const config = {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        };
+  
+        const { data } = await axios.get(`/api/user?search=${search}`, config);
+  
+        setLoading(false);
+        setSearchResult(data);
+      } catch (error) {
+        toast({
+          title: "Error Occured!",
+          description: "Failed to Load the Search Results",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+          position: "bottom-left",
+        });
+      }
+}
+
+const accessChat = (userId)=>{
+
+}
   return (
     <>
       <Box    display="flex"
@@ -23,7 +74,7 @@ function SideDrawer() {
         borderWidth="5px">
     <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
 
-      <Button variant={"ghost"}>
+      <Button variant={"ghost"} onClick={onOpen}>
       <i class="fa-solid fa-magnifying-glass"></i>
       <Text display={{base : "none" , md: "flex"}} px="4">  Search User </Text>
       </Button>
@@ -50,13 +101,45 @@ function SideDrawer() {
             <MenuItem>My Profile</MenuItem>
           </ProfileModal>
             <MenuDivider/>
-            <MenuItem>Logout</MenuItem>
+            <MenuItem onClick={logoutHandler}>Logout</MenuItem>
           </MenuList>
           </Menu>
         </div>
       </Box>
+
+
+      <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay/>
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth={"1px"}>Search Users</DrawerHeader>
+          <DrawerBody>
+          <Box display={"flex"} pb={2}>
+          <Input
+                placeholder="Search by name or email"
+                mr={2}
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <Button 
+              onClick={handleSearch}
+              >
+            Go
+              </Button>
+          </Box>
+          {loading ? (
+           <ChatLoading/>
+          ) : (
+            searchResult?.map((user)=>{
+              <UserListItem key={user._id} user={user} handleFunction={()=>accessChat(user._id)}/>
+            })
+          )}
+        </DrawerBody>
+        </DrawerContent>
+       
+      </Drawer>
     </>
   )
 }
+
 
 export default SideDrawer
